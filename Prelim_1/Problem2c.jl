4cf0c43aa8063813a13ecd83e96a75dc14621cf7
sr2264 <60451433+sr2264@users.noreply.github.com>
2020-05-12 09:59:16 -0700

    Add files via upload

---
+++ Prelim_1/Problem2c.jl
@@ -0,0 +1,33 @@
+using DifferentialEquations
+using Plots
+
+#variables
+a_x = 1.5
+b_x = 5.0
+z_x = 0.4
+n_zx = 2.7
+n_xz = 2.7
+del_z = 1.0
+x_z = 1.5
+
+function balances(dA,A,S,t)
+    dA[1] = (a_x + b_x * S)/(1 + S + (A[2]/z_x)^n_zx) - A[1]
+    dA[2] = 1/(1 + (A[1]/x_z)^n_xz) - (del_z * A[2])
+end
+
+#initialization
+tspan = (0.0,100.0)
+u0 = [0.0; 0.0]
+S = range(0.001, length = 10^4, stop = 100)
+X = zeros(length(S))
+Z = zeros(length(S))
+
+#looping to get the values of X and Z for different values of S
+for i in 1:length(S)
+    prob = ODEProblem(balances,u0,tspan,S[i])
+    solution = solve(prob)
+    X[i] = solution[1,end]
+    Z[i] = solution[2,end]
+end
+
+plot(S,X, xaxis = ("S", :log), yaxis = ("X", :log), label =("X vs S)"))
